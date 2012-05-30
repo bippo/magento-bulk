0fe3438e76886927c8e74de6b8ec2d586de80ced
Hendy Irawan <hendy@soluvas.com>
2012-05-30 16:39:55 +0700

    List product attributes is working well.

---
+++ attr-list.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'init.php';
+
+echo "List all product attributes\n";
+$attrs = Mage::getResourceModel('catalog/product_attribute_collection');
+Mage::log('attr-list: '. count($attrs) . ' attributes total');
+$attrs_data = $attrs->load();
+echo "G=global C=configurable V=visible U=user-defined\n";
+foreach ($attrs_data as $attr) {
+// 	$attr->load();
+// 	var_dump($attr->getData());
+	printf("%3d %-30s %-10s %-11s %1s%1s%1s%1s %-30s\n", $attr->getId(),
+		$attr->getAttributeCode(),
+		$attr->getBackendType(),
+		$attr->getFrontendInput(),
+		$attr->getIsGlobal() ? 'G' : '-',
+		$attr->getIsConfigurable() ? 'C' : '-',
+		$attr->getIsVisible() ? 'V' : '-',
+		$attr->getIsUserDefined() ? 'U' : '-',
+		join(',', $attr->getApplyTo()));
+	//var_dump($attr);
+	if ($attr->usesSource()) {
+		$source = $attr->getSource();
+		$optStrArr = array();
+		foreach ($source->getAllOptions() as $optionOrder => $optionValue) {
+			if (empty($optionOrder) || empty($optionValue))
+				continue;
+			$optStrArr[] = $optionValue['value'] . '='. $optionValue['label'];
+		}
+		echo "    " . join(' ', $optStrArr) ."\n";		
+	}
+	
+}
--- product-create-conf.php
+++ product-create-conf.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Create a configurable product using 'Women Clothing' attribute set, that uses 'item_color' and 'item_size' configurable attributes.
+ * The Attribute Set code is predefined.
+ * The Attr
+ */
 require_once 'init.php';
 
 echo "Create configurable product\n";
