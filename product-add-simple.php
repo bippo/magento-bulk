fb350359e07c84dcaa893d8e7db6498afd065e7a
Hendy Irawan <hendy@soluvas.com>
2012-05-31 00:29:47 +0700

    Create simple product with category. Refs #4.

--- product-add-simple.php
+++ product-add-simple.php
@@ -2,9 +2,9 @@
 <?php
 require_once 'init.php';
 
-$opts = getopt('', array('sku:', 'name:', 'price:', 'qty:', 'cat:', 'weight:', 'store:', 'set:', 'summary:', 'desc:', 'webs:'));
+$opts = getopt('', array('sku:', 'name:', 'price:', 'qty:', 'cats:', 'weight:', 'store:', 'set:', 'summary:', 'desc:', 'webs:'));
 if (empty($opts) || empty($opts['sku']) || empty($opts['name']) || empty($opts['price'])) {
-	echo "Usage: product-add-simple.php --sku SKU --name NAME --price PRICE [--qty QTY] [--cat CATEGORY_ID] [--summary SUMMARY] [--desc  DESCRIPTION] [--weight WEIGHT] [--store STORE_ID] [--set ATTRIBUTE_SET_ID] [--webs CODE,...]\n";
+	echo "Usage: product-add-simple.php --sku SKU --name NAME --price PRICE [--qty QTY] [--cats URL_KEY,...] [--summary SUMMARY] [--desc  DESCRIPTION] [--weight WEIGHT] [--store STORE_ID] [--set ATTRIBUTE_SET_ID] [--webs CODE,...]\n";
 	exit(1);
 }
 
@@ -16,6 +16,19 @@ foreach ($websites as $website) {
 }
 echo ' '. join(' ', array_keys($websiteLookup)) ."\n";
 
+echo 'Loading categories...';
+$categories = Mage::getModel('catalog/category')->getCollection()
+	->addAttributeToSelect('*');
+$categoryLookup = array();
+$defaultCategoryIds = array();
+foreach ($categories as $cat) {
+	if ($cat->getUrlKey() !== '')
+		$categoryLookup[$cat->getUrlKey()] = $cat->getId();
+	if ($cat->getLevel() <= 1 && $cat->getIsActive())
+		$defaultCategoryIds[] = $cat->getId();
+}
+echo join(' ', array_keys($categoryLookup)) .". defaults: ". join(' ', $defaultCategoryIds) . "\n";
+
 $sku = $opts['sku'];
 $name = $opts['name'];
 $price = $opts['price'];
@@ -24,7 +37,7 @@ $setId = !empty($opts['set']) ? $opts['set'] : DEFAULT_ATTRIBUTE_SET_ID;
 $summary = !empty($opts['summary']) ? $opts['summary'] : $name;
 $description = !empty($opts['desc']) ? $opts['desc'] : $summary;
 $qty = !empty($opts['qty']) ? $opts['qty'] : 1.0;
-$categoryId = !empty($opts['cat']) ? $opts['cat'] : DEFAULT_CATEGORY_ID;
+$cats = !empty($opts['cats']) ? $opts['cats'] : '';
 $weight = !empty($opts['weight']) ? $opts['weight'] : 1.0;
 $webs = !empty($opts['webs']) ? $opts['webs'] : join(',', array_keys($websiteLookup)); // if not specified, select all websites
 echo "Create simple product $sku name: $name price: $price qty: $qty webs: $webs\n";
@@ -36,6 +49,18 @@ foreach ($webCodes as $webCode) {
 		throw new Exception("Cannot find website '$webCode'");
 	$websiteIds[] = $websiteLookup[$webCode];
 }
+echo 'Website IDs: '. join(' ', $websiteIds) ."\n";
+
+$catKeys = split(',', $cats);
+$categoryIds = $defaultCategoryIds;
+foreach ($catKeys as $catKey) {
+	if (!isset($categoryLookup[$catKey]))
+		throw new Exception("Cannot find category '$catKey'");
+	$categoryId = $categoryLookup[$catKey];
+	if (!in_array($categoryId, $categoryIds))
+		$categoryIds[] = $categoryId;
+}
+echo 'Category IDs: '. join(' ', $categoryIds) ."\n";
 
 $product = Mage::getModel('catalog/product');
 $product->setStoreId($storeId)		// is Product.storeId deprecated? seems weird, bcuz Product can be assigned to multiple Websites now 
@@ -49,7 +74,7 @@ $product->setStatus(1);
 $product->setVisibility(4);
 $product->setWeight($weight);
 $product->setPrice($price);
-$product->setCategoryIds(array($categoryId));
+$product->setCategoryIds($categoryIds);
 $product->setTaxClassId(0); // 0=None 2=Taxable Goods 4=Shipping
 $product->setWebsiteIds($websiteIds);
 
