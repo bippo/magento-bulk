f3544a08f62fd12b06b7c840918e876ea171ddbb
Hendy Irawan <hendy@soluvas.com>
2012-05-31 01:07:11 +0700

    Delete all categories. Refs #4.

---
+++ cat-clear.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+require_once 'init.php';
+
+echo "Delete all categories level 2 and below\n";
+
+$categories = Mage::getModel('catalog/category')->getCollection()
+	->addAttributeToSelect('*');
+foreach ($categories as $cat) {
+	if ($cat->getLevel() >= 2) {
+		echo "Deleting #{$cat->getId()}:{$cat->getUrlKey()}:{$cat->getName()}...";
+		$cat->delete();
+		echo " DELETED.\n";
+	} else {
+		echo "Skipping #{$cat->getId()}:{$cat->getName()}...\n";
+	}
+}
