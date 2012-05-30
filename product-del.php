a9e722ccd49831349a9d8b81a39a2cde7b3cac28
Hendy Irawan <hendy@soluvas.com>
2012-05-30 21:07:56 +0700

    Delete product.

---
+++ product-del.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'init.php';
+
+if (count($argv) < 2) {
+	echo "Delete products\n";
+	echo "Usage: php product-del.php SKU [...]\n";
+	exit(1);
+}
+
+for ($i = 1; $i < count($argv); $i++) {
+	$sku = $argv[$i];
+	echo "Deleting product $sku...";
+	$product = Mage::getModel('catalog/product');
+	$product->load($product->getIdBySku($sku));
+	if ($product->getId() == false) {
+		echo " Not Found!\n";
+		continue;
+	}
+	$product->delete();
+	echo " DELETED.\n";
+}
---
+++ product-delid.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'init.php';
+
+if (count($argv) < 2) {
+	echo "Delete products by ID\n";
+	echo "Usage: php product-delid.php ID [...]\n";
+	exit(1);
+}
+
+for ($i = 1; $i < count($argv); $i++) {
+	$id = $argv[$i];
+	echo "Deleting product #$id...";
+	$product = Mage::getModel('catalog/product');
+	$product->load($id);
+	if ($product->getId() == false) {
+		echo " Not Found!\n";
+		continue;
+	}
+	$product->delete();
+	echo " DELETED.\n";
+}
