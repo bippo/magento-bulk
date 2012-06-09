63ff8966c51629ca87b5ea9cf919a83090bd7681
Hendy Irawan <hendy@soluvas.com>
2012-06-09 14:47:33 +0700

    Support both configurable and simple import. Refs #7.

--- product-import.php
+++ product-import.php
@@ -3,6 +3,30 @@
 require_once 'init.php';
 require_once 'product_functions.php';
 
+/*
+Sample input format:
+ 
+<products>
+    <product>
+        <type>configurable</type>
+        <sku>zibalabel_t03</sku>
+        <set>Women Clothing</set>
+        <!-- store is optional -->
+        <store>1</store> 
+        <name>Tas Batik T-03</name>
+        <price>38200.00</price>
+        <categories>tas,handbag</categories>
+        <!-- webs is optional -->
+        <webs>tas,handbag</webs>
+        <weight>1.0</weight>
+        <summary>Tas Trendy Banyak Gaya</summary>
+        <description>Tas yang sangat praktis dipakai ke mana-mana.</description>
+        <variants>Hijau/S:43,Hijau/M:20,Merah/S:12,Merah/M:15</variants>
+    </product>
+    ...
+</products>
+*/
+
 if (count($argv) < 2) {
 	echo "Import products\n";
 	echo "Usage: product-import.php INPUT_XML\n";
@@ -72,20 +96,18 @@ $xmlFilename = $argv[1];
 echo "Loading $xmlFilename...";
 $product_xml = simplexml_load_file($xmlFilename);
 echo " Loaded.\n";
-foreach ($product_xml as $product ) {
-	$storeId = !empty($product->storeId) ? $product->storeId : DEFAULT_STORE_ID;
+foreach ($product_xml as $product) {
+	$storeId = !empty($product->store) ? $product->store : DEFAULT_STORE_ID;
 	$sku = $product->sku;
 	$name = $product->name;
 	$price = trim($product->price);
 	$variants = $product->variants;
 	$set = $product->set;
-	$desc = $product->desciption;
+	$description = $product->description;
 	$summary = $product->summary;
 	$cats = $product->categories;
 	$webs = $product->webs;
 	
-	echo "Create configurable product $sku name: $name set: $set price: $price variants: $variants cats: $cats webs: $webs\n";
-
 	// Determine website IDs
 	$webCodes = !empty($webs) && $webs != '-' ? explode(',', $webs) : array();
 	$websiteIds = array();
@@ -113,44 +135,10 @@ foreach ($product_xml as $product ) {
 		throw new Exception("Cannot find attribute set '$set'");
 	$setId = $sets[$set]->getId();
 	echo "Attribute set ID: $setId\n";
-	
-	$variantCodes = explode(',', $variants);
-	
-	// Generate the child products
-	$variantsData = array();
-	foreach ($variantCodes as $variantCode) {
-		if (!preg_match('/^(.+)\\/(.+):(.+)$/', $variantCode, $matches))
-			throw new Exception("Invalid variant code: $variantCode");
-		list($dummy, $color, $size, $qty) = $matches;
-		//var_dump($optionLookup['item_color']);
-		echo "Varian Code Test => ". $variantCode."\n";
-		$color = str_replace("_", " ", $color);
-		echo "Test Color --> " . $optionLookup['item_color'][$color]."\n";
-		if (!isset($optionLookup['item_color'][$color])) {
-			throw new Exception("Cannot find option value for item_color '$color'");
-		}
-		if (!isset($optionLookup['item_size'][$size])) {
-			throw new Exception("Cannot find option value for item_size '$size'");
-		}
-		$sizeId = $optionLookup['item_size'][$size];
-		$variantSku = $sku .' - '. $color .'_'. $size;
-		$variantName = $name .' - '. $color .'_'. $size;
-		echo "Variant $variantSku $variantName: qty=$qty item_color=$colorId:$color item_size=$sizeId:$size\n";
-		$variantsData[] = array(
-				'sku' => $variantSku,
-				'name' => $variantName,
-				'qty' => $qty,
-				'item_color' => $colorId,
-				'item_size' => $sizeId
-		);
-	}
 
-	// Create the childs products and configurable product
-	createConfigurableProduct(
-		array(
-			'item_color_attrId' => $item_color_attrId,
-			'item_size_attrId'  => $item_size_attrId),
-		array(
+	if ($product->type == 'simple') {
+		echo "Create simple product $sku name: $name price: $price qty: $qty cats: $cats webs: $webs\n";
+		createSimpleProduct(array(
 			'storeId'		=> $storeId,
 			'setId'			=> $setId,
 			'sku'			=> $sku,
@@ -160,7 +148,60 @@ foreach ($product_xml as $product ) {
 			'weight'		=> $weight,
 			'price'			=> $price,
 			'categoryIds'	=> $categoryIds,
-			'websiteIds'	=> $websiteIds),
-		$variantsData);
+			'websiteIds'	=> $websiteIds));
+	} else if ($product->type == 'configurable') {
+		echo "Create configurable product $sku name: $name set: $set price: $price variants: $variants cats: $cats webs: $webs\n";
+		
+		$variantCodes = explode(',', $variants);
+		
+		// Generate the child products
+		$variantsData = array();
+		foreach ($variantCodes as $variantCode) {
+			if (!preg_match('/^(.+)\\/(.+):(.+)$/', $variantCode, $matches))
+				throw new Exception("Invalid variant code: $variantCode");
+			list($dummy, $color, $size, $qty) = $matches;
+			//var_dump($optionLookup['item_color']);
+			echo "Varian Code Test => ". $variantCode."\n";
+			$color = str_replace("_", " ", $color);
+			echo "Test Color --> " . $optionLookup['item_color'][$color]."\n";
+			if (!isset($optionLookup['item_color'][$color])) {
+				throw new Exception("Cannot find option value for item_color '$color'");
+			}
+			if (!isset($optionLookup['item_size'][$size])) {
+				throw new Exception("Cannot find option value for item_size '$size'");
+			}
+			$sizeId = $optionLookup['item_size'][$size];
+			$variantSku = $sku .' - '. $color .'_'. $size;
+			$variantName = $name .' - '. $color .'_'. $size;
+			echo "Variant $variantSku $variantName: qty=$qty item_color=$colorId:$color item_size=$sizeId:$size\n";
+			$variantsData[] = array(
+					'sku' => $variantSku,
+					'name' => $variantName,
+					'qty' => $qty,
+					'item_color' => $colorId,
+					'item_size' => $sizeId
+			);
+		}
+	
+		// Create the childs products and configurable product
+		createConfigurableProduct(
+			array(
+				'item_color_attrId' => $item_color_attrId,
+				'item_size_attrId'  => $item_size_attrId),
+			array(
+				'storeId'		=> $storeId,
+				'setId'			=> $setId,
+				'sku'			=> $sku,
+				'name'			=> $name,
+				'summary'		=> $summary,
+				'description'	=> $description,
+				'weight'		=> $weight,
+				'price'			=> $price,
+				'categoryIds'	=> $categoryIds,
+				'websiteIds'	=> $websiteIds),
+			$variantsData);
+	} else {
+		throw new Exception("Unknown product type: {$product->type} for {$product->sku}\n");
+	}
 
 }
